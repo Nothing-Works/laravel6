7638085f4a59f9186de68c146eb46fe4f6887ca1
AndySong <andylauszp@gmail.com>
2020-01-30 11:09:16 +1300

    markdown email

--- app/Mail/ContactMe.php
+++ app/Mail/ContactMe.php
@@ -30,6 +30,9 @@ class ContactMe extends Mailable
      */
     public function build()
     {
-        return $this->view('emails.contact-me')->subject('More information about '.$this->topic);
+        //use view to do email
+        // return $this->view('emails.contact-me')->subject('More information about '.$this->topic);
+        //use markdown to do email
+        return $this->markdown('emails.contact-meMD')->subject('More information about '.$this->topic);
     }
 }
---
+++ resources/views/emails/contact-meMD.blade.php
@@ -0,0 +1,10 @@
+@component('mail::message')
+# A Heading
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur recusandae ullam, consequuntur architecto labore
+repellendus odio rem? Totam id a impedit, mollitia, nemo ut repudiandae aut reprehenderit maiores eligendi neque.
+
+- A list
+- goes
+- here
+@endcomponent
