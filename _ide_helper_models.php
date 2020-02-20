93ab9a811fd09f9026a73b621c49b5f80b794019
AndySong <andylauszp@gmail.com>
2020-02-21 10:45:55 +1300

    cors

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -14,26 +14,12 @@ namespace App{
 /**
  * App\Article
  *
- * @property int $id
- * @property int $user_id
- * @property string $title
- * @property string $excerpt
- * @property string $body
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Tag[] $tags
  * @property-read int|null $tags_count
  * @property-read \App\User $user
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Article newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Article newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Article query()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Article whereBody($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Article whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Article whereExcerpt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Article whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Article whereTitle($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Article whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Article whereUserId($value)
  */
 	class Article extends \Eloquent {}
 }
@@ -42,21 +28,9 @@ namespace App{
 /**
  * App\Assignment
  *
- * @property int $id
- * @property string $body
- * @property string|null $completed_at
- * @property string|null $due_date
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Assignment newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Assignment newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Assignment query()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Assignment whereBody($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Assignment whereCompletedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Assignment whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Assignment whereDueDate($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Assignment whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Assignment whereUpdatedAt($value)
  */
 	class Assignment extends \Eloquent {}
 }
@@ -65,23 +39,9 @@ namespace App{
 /**
  * App\Post
  *
- * @property int $id
- * @property string $slug
- * @property string $title
- * @property string $body
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property string|null $published_at
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Post newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Post newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Post query()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Post whereBody($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Post whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Post whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Post wherePublishedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Post whereSlug($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Post whereTitle($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Post whereUpdatedAt($value)
  */
 	class Post extends \Eloquent {}
 }
@@ -90,16 +50,10 @@ namespace App{
 /**
  * App\Project
  *
- * @property int $id
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
  * @property-read \App\User $user
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Project newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Project newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Project query()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Project whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Project whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Project whereUpdatedAt($value)
  */
 	class Project extends \Eloquent {}
 }
@@ -108,19 +62,11 @@ namespace App{
 /**
  * App\Tag
  *
- * @property int $id
- * @property string $name
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Article[] $articles
  * @property-read int|null $articles_count
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Tag newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Tag newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Tag query()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Tag whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Tag whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Tag whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\Tag whereUpdatedAt($value)
  */
 	class Tag extends \Eloquent {}
 }
@@ -129,14 +75,6 @@ namespace App{
 /**
  * App\User
  *
- * @property int $id
- * @property string $name
- * @property string $email
- * @property \Illuminate\Support\Carbon|null $email_verified_at
- * @property string $password
- * @property string|null $remember_token
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Article[] $articles
  * @property-read int|null $articles_count
  * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
@@ -146,14 +84,6 @@ namespace App{
  * @method static \Illuminate\Database\Eloquent\Builder|\App\User newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|\App\User newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|\App\User query()
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereEmail($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereEmailVerifiedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User wherePassword($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereRememberToken($value)
- * @method static \Illuminate\Database\Eloquent\Builder|\App\User whereUpdatedAt($value)
  */
 	class User extends \Eloquent {}
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,5 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/stats', fn () => ['name' => 'andy', 'age' => 30]);
